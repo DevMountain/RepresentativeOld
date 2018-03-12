5543fe8d8104425b74bec3853c71aca6d44627d0
Andrew Madsen <andrew@openreelsoftware.com>
2018-03-12 11:36:18 -0600

    Fix failure to decode reps for ID and CA

--- Representative/RepresentativeController.swift
+++ Representative/RepresentativeController.swift
@@ -10,45 +10,57 @@ import Foundation
 
 class RepresentativeController {
 	
-	static let baseURL = URL(string: "http://whoismyrepresentative.com/getall_reps_bystate.php")!
+	static let baseURL = URL(string: "https://whoismyrepresentative.com/getall_reps_bystate.php")!
 	
 	static let shared = RepresentativeController()
 	
-    func searchRepresentatives(forState state: String, completion: @escaping (_ representatives: [Representative]) -> Void) {
-        
-        let url = RepresentativeController.baseURL
-        let urlParameters = ["state": "\(state)", "output": "json"]
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
-        let queryItems = urlParameters.flatMap({ URLQueryItem(name: $0.key, value: $0.value) })
-        components?.queryItems = queryItems
-        
-        guard let requestURL = components?.url else { completion([]); return }
-
-        let dataTask = URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
-            
-            if let error = error {
-                NSLog("Unable to get representatives for \(state): \(error)")
-                completion([])
-                return
-            }
-            guard let data = data, let responseDataString = String(data: data, encoding: .utf8) else {
-                NSLog("Unable to get representatives for \(state): No data returned.")
-                completion([])
-                return
-            }
-            
-            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: Any],
-                let representativeDictionaries = jsonDictionary["results"] as? [[String: Any]] else {
-                    NSLog("Unable to serialize json. \nResponse: \(responseDataString)")
-                        completion([])
-                        return
-                }
-                
-                let representatives = representativeDictionaries.flatMap { Representative(json: $0) }
-                completion(representatives)
-        }
-        
-        dataTask.resume()
-    }
+	func searchRepresentatives(forState state: String, completion: @escaping (_ representatives: [Representative]) -> Void) {
+		
+		let url = RepresentativeController.baseURL
+		let urlParameters = ["state": "\(state)", "output": "json"]
+		var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+		let queryItems = urlParameters.flatMap({ URLQueryItem(name: $0.key, value: $0.value) })
+		components?.queryItems = queryItems
+		
+		guard let requestURL = components?.url else { completion([]); return }
+		
+		let dataTask = URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+			
+			if let error = error {
+				NSLog("Unable to get representatives for \(state): \(error)")
+				completion([])
+				return
+			}
+			
+			/*
+			whoismyrepresentative.com incorrectly encodes letters with diacrtic marks, e.g. ú using
+			extended ASCII, not UTF-8. This means that trying to convert the data to a string using .utf8 will
+			fail for some states, where the representatives have diacritics in their names.
+			
+			To workaround this, we decode into a string using ASCII, then reencode the string as data using .utf8
+			before passing the fixed UTF-8 data into the JSON decoder.
+			*/
+			
+			guard let data = data,
+				let responseDataString = String(data: data, encoding: .ascii),
+				let fixedData = responseDataString.data(using: .utf8) else { // Convert string back to UTF8 data
+					NSLog("Unable to get representatives for \(state): No data returned.")
+					completion([])
+					return
+			}
+			
+			guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: fixedData, options: [])) as? [String: Any],
+				let representativeDictionaries = jsonDictionary["results"] as? [[String: Any]] else {
+					NSLog("Unable to deserialize json. \nResponse: \(responseDataString)")
+					completion([])
+					return
+			}
+			
+			let representatives = representativeDictionaries.flatMap { Representative(json: $0) }
+			completion(representatives)
+		}
+		
+		dataTask.resume()
+	}
 }
 
